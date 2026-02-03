bd6fa919e385b7e3de2140adfb50b40e7fd7d2e3
Pedro Henrique <pedroespindola2912@gmail.com>
2026-02-03 20:59:51 -0300

    feat: comecando navbar e botoes

---
+++ src/components/ButtonContact.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <button
+        class="group relative w-48 h-14 overflow-hidden rounded-full bg-green-600 text-white shadow-lg transition-all duration-500 hover:bg-green-700 hover:shadow-xl cursor-pointer">
+        <div class="relative h-full w-full flex items-center justify-center">
+
+            <span
+                class="absolute transform transition-all duration-500 ease-out group-hover:-translate-x-10 group-hover:opacity-0 font-bold text-xl">
+                Contatos
+            </span>
+
+            <span
+                class="absolute transform translate-x-10 opacity-0 transition-all duration-500 ease-out group-hover:translate-x-0 group-hover:opacity-100 flex items-center justify-center">
+                <svg class="h-7 w-7 rotate-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="22" y1="2" x2="11" y2="13"></line>
+                    <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+                </svg>
+            </span>
+
+        </div>
+    </button>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ButtonContact from './ButtonContact.vue';
+
+</script>
+
+<template>
+    <nav class="bg-sky-900 h-[130px] relative flex items-center px-12">
+
+        <div class="flex-1"></div>
+
+        <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <img src="/images/logoepimanager.png" alt="Logo Epi Manager" class="h-[300px] w-auto object-contain">
+        </div>
+
+        <div class="flex-1 flex justify-end">
+            <ButtonContact />
+        </div>
+
+    </nav>
+</template>
